790104c6b8440c85700447140b69678b12007e2d
Sourabh Suman <sourabh2812@live.com>
2017-06-05 11:52:22 +0200

    Initial commit

---
+++ RouteMapProto/WebContent/scripts/data.js
@@ -0,0 +1,145 @@
+eqfeed_callback({
+	"type" : "FeatureCollection",
+	"features" : [ {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 10.7522, 59.9139 ],
+			"title" : "Oslo"
+		},
+		"destinations" : [ {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 51.508530,
+			lng : -0.076132
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 40.730610,
+			lng : -73.935242
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 48.8566,
+			lng : 2.3522
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 50.1109,
+			lng : 8.6821
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 60.192059,
+			lng : 24.945831
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 59.334591,
+			lng : 18.063240
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		}, {
+			lat : 55.676098,
+			lng : 12.568337
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 0.1278, 51.5074 ],
+			"title" : "London"
+		},
+		"destinations" : [ {
+			lat : 51.5074,
+			lng : 0.1278
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 24.945831, 60.192059 ],
+			"title" : "Helsingfors"
+		},
+		"destinations" : [ {
+			lat : 60.192059,
+			lng : 24.945831
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 18.063240, 59.334591 ],
+			"title" : "Stockholm"
+		},
+		"destinations" : [ {
+			lat : 59.334591,
+			lng : 18.063240
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 12.568337, 55.676098 ],
+			"title" : "København"
+		},
+		"destinations" : [ {
+			lat : 55.676098,
+			lng : 12.568337
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ -73.935242, 40.730610 ],
+			"title" : "NY"
+		},
+		"destinations" : [ {
+			lat : 40.730610,
+			lng : -73.935242
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 8.6821, 50.1109 ],
+			"title" : "Frankfurt"
+		},
+		"destinations" : [ {
+			lat : 50.110924,
+			lng : 8.682127
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	}, {
+
+		"geometry" : {
+			"type" : "Point",
+			"coordinates" : [ 2.3522, 48.8566 ],
+			"title" : "Paris"
+		},
+		"destinations" : [ {
+			lat : 48.8566,
+			lng : 2.3522
+		}, {
+			lat : 59.911491,
+			lng : 10.757933
+		} ]
+	} ]
+});
